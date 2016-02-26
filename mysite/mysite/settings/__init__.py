3cbc81a467b5e6be2614e7a9a4405470fb95c1d1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-02-26 11:19:52 +0100

    Refactorise settings modules

---
+++ mysite/mysite/settings/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from .base import *
+# vim:set et sts=4 ts=4 tw=80:
