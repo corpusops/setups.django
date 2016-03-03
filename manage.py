671b78556123b6c1501bdf0c80eba78dc29b3411
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-03-03 11:22:22 +0100

    for conveninence, really annoying to enter --settings flag everytime

--- manage.py
+++ manage.py
@@ -3,7 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
+
+    mod_settings = "mysite.settings.dev"
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        mod_settings = "mysite.settings.test"
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", mod_settings)
 
     from django.core.management import execute_from_command_line
 
