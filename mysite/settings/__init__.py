afa23cb0425c2e2d243e78df5f29dc8f79405e97
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-03-03 11:22:51 +0100

    THAT the fucntion I would like to use for importing env settings with pre and post hook and prevent duplicate ugly codes but... It is now working on py2 at least

--- mysite/settings/__init__.py
+++ mysite/settings/__init__.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
-from .base import *
+from importlib import import_module
+
+
+def load_env_settings(env, hook='post'):
+    """
+    For a given hook (pre or post), import default and (then) specific
+    environment settings override.
+    """
+    patterns = ['.local.{hook}.base', '.local.{hook}.{env}']
+
+    for pattern in patterns:
+        name = pattern.format(hook=hook, env=env)
+
+        try:
+            import_module(name, __name__)
+        except ImportError:
+            pass
+        else:
+            globals().update(locals())
