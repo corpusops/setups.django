481d9fb1e53cd627b0cd4d8506ae5a63e4985ba6
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-07-07 12:23:56 +0200

    layout review

--- src/manage.py
+++ src/manage.py
@@ -1,1 +1,17 @@
-../manage.py
+#!/usr/bin/env python
+import os
+import sys
+
+# Set the default settings
+mod_settings = 'project.settings.dev'
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    mod_settings = 'project.settings.test'
+
+
+if __name__ == "__main__":
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", mod_settings)
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
