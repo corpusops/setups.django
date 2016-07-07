97b0c0283a745de5394e9515303ae70d81bca432
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-07-07 15:51:32 +0200

    add default login logout and homepage

---
+++ src/common/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'common/home.html'
