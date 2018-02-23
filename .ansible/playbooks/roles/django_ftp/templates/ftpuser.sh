121c243580fe069aadfd0a390db086e703bbce34
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-02-23 16:19:11 +0100

    more ftp resilient code

--- .ansible/playbooks/roles/django_ftp/templates/ftpuser.sh
+++ .ansible/playbooks/roles/django_ftp/templates/ftpuser.sh
@@ -1,31 +1,37 @@
 #!/usr/bin/env bash
 set -x
-{% if delete  %}
-if ( pure-pw show "{{user}}" >/dev/null 2>&1 );then
-  set -e
-  pure-pw userdel "{{user}}"
-fi
-{% else %}
-uid=$(id -u {{sysuser}})
-gid=$(getent group {{sysgroup}} | cut -d: -f3)
-f=$(mktemp)
-echo prout $f
-cat > "$f" << EOF
-{{password}}
-{{password}}
+USER="{{user}}"
+PASSWORD="{{password}}"
+UHOME="{{home}}"
+DELETE="{% if delete%}y{%endif %}"
+ret=0
+if [ "x$DELETE" != "x" ];then
+    if ( pure-pw show "$USER" >/dev/null 2>&1 );then
+        pure-pw userdel "$USER"
+        ret=$?
+    fi
+else
+    uid=$(id -u {{sysuser}})
+    gid=$(getent group {{sysgroup}} | cut -d: -f3)
+    f=$(mktemp)
+    cat > "$f" << EOF
+$PASSWORD
+$PASSWORD
 EOF
-cat $f
-echo here
-pure-pw show "{{user}}" 2>&1 || /bin/true
-if ! ( pure-pw show "{{user}}" >/dev/null 2>&1 );then
-  set -e
-  pure-pw useradd "{{user}}" -d "{{home}}" -u $uid -g $gid -m < "$f"
+    pure-pw show "$USER" 2>&1 || /bin/true
+    if ! ( pure-pw show "$USER" >/dev/null 2>&1 );then
+      cat "$f" | pure-pw useradd "$USER" -d "$UHOME" -u $uid -g $gid -m
+      ret=$?
+    fi
+    if ( pure-pw show "$USER" >/dev/null 2>&1 );then
+      cat "$f" | pure-pw usermod "$USER" -d "$UHOME" -u $uid -g $gid -m
+      ret=$?
+    fi
+    if [ "x$ret" != "x0" ];then
+        cat "$f" | pure-pw passwd "$USER"
+        ret=$?
+    fi
+    rm -f "${f}"
 fi
-if ( pure-pw show "{{user}}" >/dev/null 2>&1 );then
-  set -e
-  pure-pw usermod "{{user}}" -d "{{home}}" -u $uid -g $gid -m < "$f"
-  pure-pw passwd "{{user}}" < "$f"
-fi
-rm -f "${f}"
-{% endif %}
+exit $ret
 # vim:set et sts=4 ts=4 tw=80:
